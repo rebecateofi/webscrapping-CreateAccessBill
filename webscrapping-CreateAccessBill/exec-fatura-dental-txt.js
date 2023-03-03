102f3570196e3c5d92b9e030b4f11ba985dee21f
felipe.rodrigues <felipe.rodrigues@LNOT-5FGVHF3>
2023-03-03 07:49:43 -0300

    use CreateFolderIfItDoesNotExists method

--- webscrapping-CreateAccessBill/exec-fatura-dental-txt.js
+++ webscrapping-CreateAccessBill/exec-fatura-dental-txt.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const Crawler = require('crawler');
 const fs = require("fs");
 const express = require("express");
-const { IsApplicationBlocked } = require("./utility/functions");
+const { IsApplicationBlocked, CreateFolderIfItDoesNotExists } = require("./utility/functions");
 
 {
 	const app = express()
@@ -34,9 +34,9 @@ async function ExecuteWebScraping(users) {
 	for (const user of users) {
 		const page = await browser.newPage();
 		var typing = 'Fatura Dental csv';
-		if (!fs.existsSync('C:\\Faturas-automatizado\\' + typing)) {
-			fs.mkdirSync('C:\\Faturas-automatizado\\' + typing);
-		}
+
+		CreateFolderIfItDoesNotExists('C:\\Faturas-automatizado\\' + typing);
+
 		try {
 			await page.waitForTimeout(2000);
 			await page.goto('https://www.hapvida.com.br/pls/podontow/webNewDentalEmpresarial.pr_login_empresa_opmenu?pOpMenu=8');
@@ -105,12 +105,11 @@ async function ExecuteWebScraping(users) {
 										} else {
 											var groupName = 'C:\\Faturas-automatizado\\' + typing + '\\'+ user[6];
 											var enterpriseName = 'C:\\Faturas-automatizado\\' + typing + '\\'+ user[6] + '\\' + user[7];
-											if (!fs.existsSync(groupName)){
-												fs.mkdirSync(groupName);
-											}
-											if(!fs.existsSync(enterpriseName)){
-												fs.mkdirSync(enterpriseName);
-											}
+											
+											CreateFolderIfItDoesNotExists(groupName);
+
+											CreateFolderIfItDoesNotExists(enterpriseName);
+
 											if(sendDate == '20' || sendDate == '25'){
 												fs.appendFile('C:\\Faturas-automatizado\\' + typing + '\\' + user[6] + '\\' + user[7] + '\\' + (dateNow.getMonth()+2) + ' - FATURA ' + user[1] + '.csv', res.body, function (err) {
 													if (err) console.log(err.message);
