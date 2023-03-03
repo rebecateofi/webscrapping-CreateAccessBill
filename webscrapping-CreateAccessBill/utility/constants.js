ab8cc78b5868ee651c55178bf7cc1c87561bc166
felipe.rodrigues <felipe.rodrigues@LNOT-5FGVHF3>
2023-03-02 21:01:14 -0300

    create IsApplicationBlocked method in functions.js file

---
+++ webscrapping-CreateAccessBill/utility/constants.js
@@ -0,0 +1,7 @@
+const PASTA_GERAR_FATURA_AUTOMATIZADA = 'C:\\Faturas-automatizado\\';
+const ARQUIVO_ACESSOS = 'acesso.csv';
+
+module.exports = {
+    PASTA_GERAR_FATURA_AUTOMATIZADA,
+    ARQUIVO_ACESSOS
+};
